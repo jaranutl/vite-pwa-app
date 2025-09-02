e201674c352698d548759da842645bd786fa368b
Jaranut Lertsirivorakul <jaranut@hotmail.com>
2025-09-02 21:20:35 +0700

    refactor: update MyReserve and Settings components for consistency and readability

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 export const Settings = () => {
   return (
     <div>
-      <p className='text-xl'>
-        Settings
-      </p>
-      
+      <p className="text-xl m-5">Settings</p>
+      <ul className="menu bg-base-200 rounded-box w-auto">
+        <li>
+          <a>
+            <AccountCircleOutlinedIcon />
+            My Account
+          </a>
+        </li>
+        <li>
+          <a>About Us</a>
+        </li>
+        <li>
+          <a>About this App</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
     </div>
-  )
-}
-
+  );
+};
